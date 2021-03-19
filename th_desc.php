4bf24774e71cbd3626ea153bafcdb8a033967ba8
Tima <timokha_98@mail.ru>
2021-03-20 00:13:23 +0600

    new file

--- th_desc.php
+++ th_desc.php
@@ -4,18 +4,44 @@
 
 echo `<h3>`.$_GET['name'].`</h3>`;
 
-
+//выводит даты
 if(isset($_GET["id"])){
-    $cit=$_GET["id"];
-    $que=mysqli_query($con, "select * from tbl_shows WHERE `id`='" . $cit );
-    while($m=mysqli_fetch_array($que))
-    {
-    echo '
-    <div class="theatre">
-    <a href=#>'.$m['name'].'</a>
-    <p class="address">Address</p>
-    </div>';
-}}
+    $theatre_id=$_GET["id"];
+    $que=mysqli_query($con, "select * from tbl_shows WHERE theatre_id=" . $theatre_id ." AND `status` = 1");
+    while($m=mysqli_fetch_array($que)){
+        echo '<br>';
+        echo '<a href="google.com">'.$m['start_date'].'</a>';
+    }
+    if(!isset($_GET["start_date"])){
+        $que=mysqli_query($con, "select * from tbl_shows WHERE theatre_id=" . $theatre_id ." AND `status` = 1");
+        $start_date = mysqli_fetch_array($que);
+        $start_date = $start_date["start_date"];
+    }else{
+        $start_date = $_GET["start_date"];
+    }
+}
+
+$que=mysqli_query($con, "select * from tbl_shows WHERE theatre_id=" . $theatre_id ." AND `status` = 1 AND start_date='".$start_date."'");
+while($m=mysqli_fetch_array($que)){
+    echo '<br>';
+    $movie_id = $m['movie_id'];
+        $que2=mysqli_query($con, "select * from tbl_movie WHERE movie_id=" . $movie_id );
+        $movie = mysqli_fetch_array($que2);
+        $movie_name = $movie["movie_name"];
+        $movie_image = $movie['image'];
+        echo $movie_name.'<br>';
+        echo $movie_image.'<br>';
+        $que3=mysqli_query($con, "select st_id from tbl_shows WHERE theatre_id=" . $theatre_id ." AND `status` = 1 AND movie_id=".$movie_id." AND start_date='".$start_date."'");
+        while($st_ids=mysqli_fetch_array($que3)){
+            echo '<br>';
+            $st_id=$st_ids['st_id'];
+            $que4=mysqli_fetch_array(mysqli_query($con, "select start_time from tbl_show_time WHERE st_id=" . $st_id));
+            print_r($que4['start_time']);
+
+
+        }
+}
+
 
 ?>
 <?php include('footer.php');?>
